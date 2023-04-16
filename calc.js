2e47e2b5aaddaca821ac577dfaa8309c788f5475
ronaldosilvadev <ronaldomarkhels@gmail.com>
2023-04-15 23:23:38 -0300

    Primeiro commit do meu projeto

---
+++ calc.js
@@ -0,0 +1,47 @@
+/* let nome = 'José'; 
+let sobrenome = 'da Silva';
+let idade = 27;
+let peso = 83.5;
+let altura = 1.70;
+let plano = true;
+let imc = peso/(altura**2) ;
+
+imc = imc.toFixed(2);
+
+console.log(`${nome}${sobrenome}\ntem ${idade} anos\ne seu índice de massa corporal é de ${imc}`); */
+
+/* let nome = 'Carlos '; 
+let sobrenome = 'de Souza';
+let idade = 28;
+let peso = 80.1;
+let altura = 1.76;
+let plano = 'diamante';
+let imc = peso/(altura**2) ;
+
+imc = imc.toFixed(2);
+
+console.log(`${nome}${sobrenome}\ntem ${idade} anos\ne seu índice de massa corporal é de ${imc}`);
+ */
+/* let nome = 'Aline'; 
+let sobrenome = ' Ferreira';
+let idade = 33;
+let peso = 63.7;
+let altura = 1.53;
+let plano = 'não possue';
+let imc = peso/(altura**2) ;
+
+imc = imc.toFixed(2);
+
+console.log(`${nome}${sobrenome}\ntem ${idade} anos\ne seu índice de massa corporal é de ${imc}`);
+ */
+let nome = 'Ana '; 
+let sobrenome = 'Paula';
+let idade = 26;
+let peso = 55.0;
+let altura = 1.62;
+let plano = 'prata';
+let imc = peso/(altura**2) ;
+
+imc = imc.toFixed(2);
+
+console.log(`${nome}${sobrenome}\ntem ${idade} anos\ne seu índice de massa corporal é de ${imc}`);
